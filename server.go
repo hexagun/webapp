0277a54721f363a08eb7cf639aaeef5f96e64adc
Mathieu Moretti <mormmorm@hotmail.com>
2024-06-29 22:26:54 -0400

    Reading config file with viper + configurating port to listen to

--- server.go
+++ server.go
@@ -5,10 +5,29 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/spf13/viper"
 )
 
-func main() {
+func setConfig() {
+	viper.SetConfigName("config")  // name of config file (without extension)
+	viper.SetConfigType("yaml")    // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/config") // path to look for the config file in
+	viper.AddConfigPath(".")       // optionally look for config in the working directory
 
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+		} else {
+			// Config file was found but another error was produced
+		}
+	}
+
+	viper.SetDefault("webapp.port", 8888)
+}
+
+func main() {
+	setConfig()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
@@ -17,5 +36,6 @@ func main() {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	port := fmt.Sprintf("%s%d", ":", viper.GetInt("webapp.port"))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
